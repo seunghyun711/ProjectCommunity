6875969d7fdb50b41e20cbe8ba38cf3007d4a158
hdXian <hyun353500@naver.com>
2022-10-30 18:10:51 +0900

    c221030

--- src/main/java/ctoy/projectcommunity/controller/MemberController.java
+++ src/main/java/ctoy/projectcommunity/controller/MemberController.java
@@ -23,4 +23,26 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/project/login")
+    public String loginForm() {
+        return "project/loginForm";
+    }
+
+    // longinForm에서 입력한 정보를 받아 로그인 성패여부 결정.
+    @PostMapping(value = "/project/tryLogin")
+    public String tryLogin(Member member) {
+        Member tmp = memberService.memberInfo(member.getId());
+        if(tmp == null) {
+            return "redirect:/project/loginForm";
+        }
+        else if (tmp.getId() == member.getId()) {
+            // 아직 페이지 안만듦. 로그인 성공시 글 목록으로 이동.
+            return "project/viewList";
+        }
+        else {
+            return "redirect:/project/loginForm";
+        }
+
+    }
+
 }
