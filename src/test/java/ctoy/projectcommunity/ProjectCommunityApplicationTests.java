d72af0ba7c9a9fe0e3b75f54d3d311a2d40f43a6
seunghyun711 <seunghyun711@gmail.com>
2022-11-14 21:34:24 +0900

    2022.11.14

--- src/test/java/ctoy/projectcommunity/ProjectCommunityApplicationTests.java
+++ src/test/java/ctoy/projectcommunity/ProjectCommunityApplicationTests.java
@@ -1,13 +1,13 @@
-package ctoy.projectcommunity;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjectCommunityApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ctoy.projectcommunity;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjectCommunityApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
